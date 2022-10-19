2f22ef798503e1ee12021003b854e3163ef513b3
Giovani <inf_giovani@hotmail.com>
2022-10-19 11:51:08 -0300

    commit 18/10

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,179 +5,195 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-class HomePage extends StatefulWidget{
-  const HomePage ({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+const HomePage({Key? key}) : super(key: key);
 
-  _HomePageState createState() => _HomePageState();
+@override
+_HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   final _linhas = <String>[];
-  StreamSubscription<Position>?  _subiscription;
-  Position? _ultimaLocalizacaoObtida;
-  double distanciaPercorida = 0;
+  StreamSubscription<Position>? _subscription;
+  Position? _ultimaPosicaoObtida;
+  double _distanciaTotalPercorrida = 0;
 
-  final LocationSettings locationSettings = const LocationSettings(
-    accuracy: LocationAccuracy.high,
-    distanceFilter: 20,
-    timeLimit: Duration(seconds: 2)
-  );
-
-  bool get _monitorandoLocalizacao => _subiscription != null;
+  bool get _monitorandoLocalizacao => _subscription != null;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text('Usando GPS'),
+        title: const Text('Usando GPS'),
       ),
       body: _criarBody(),
     );
   }
 
   Widget _criarBody() => Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ElevatedButton(
-              onPressed: _obterUltimaLocalizacao,
-              child: const Text('Obter a ultima localização conhecida (cache)')),
-          ElevatedButton(
-              onPressed: _obterLocalizacaoAtual,
-              child: Text('Obter localização Atual')),
-          ElevatedButton(
-              onPressed: _monitorandoLocalizacao ? _pararMonitoramento : _iniciarMonitoramento,
-              child: Text(_monitorandoLocalizacao ? 'Parar Monitoramento' : 'Iniciar Monitoramento')),
-          ElevatedButton(
-              onPressed: _limparLog,
-              child: const Text('Limpar Log')),
-          const Divider(),
-          Expanded(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: _linhas.length,
-                  itemBuilder: (_, index) => Padding(
-                    padding:  const EdgeInsets.all(5),
-                    child: Text(_linhas[index]),
-                  ),
-              ),
+    padding: const EdgeInsets.all(10),
+    child: Column(
+      children: [
+        ElevatedButton(
+          child: const Text('Obter última localização conhecida'),
+          onPressed: _obterUltimaLocalizacao,
+        ),
+        ElevatedButton(
+          child: const Text('Obter localização atual'),
+          onPressed: _obterLocalizacaoAtual,
+        ),
+        ElevatedButton(
+          child: Text(_monitorandoLocalizacao
+              ? 'Parar monitoramento'
+              : 'Monitorar localização do usuário'),
+          onPressed: _monitorandoLocalizacao
+              ? _pararMonitoramento
+              : _monitorarLocalizacao,
+        ),
+        ElevatedButton(
+          child: const Text('Limpar log'),
+          onPressed: _limparLog,
+        ),
+        const Divider(),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: _linhas.length,
+            itemBuilder: (_, index) => Padding(
+              padding: const EdgeInsets.all(5),
+              child: Text(_linhas[index]),
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
+    ),
   );
+
   void _obterUltimaLocalizacao() async {
     bool permissoesPermitidas = await _permissoesPermitidas();
-    if(!permissoesPermitidas){
+    if (!permissoesPermitidas) {
       return;
     }
-    Position? position  = await Geolocator.getLastKnownPosition();
+    Position? position = await Geolocator.getLastKnownPosition();
     setState(() {
-      if(position == null){
+      if (position == null) {
         _linhas.add('Nenhuma localização registrada');
-      }else {
-        _linhas.add('Latitude: ${position?.latitude} | Longitude: ${position
-            ?.longitude}');
+      } else {
+        _linhas.add(
+            'Latitude: ${position.latitude} | Longitude: ${position.longitude}');
       }
     });
   }
 
   void _obterLocalizacaoAtual() async {
     bool servicoHabilitado = await _servicoHabilitado();
-    if(!servicoHabilitado){
+    if (!servicoHabilitado) {
       return;
     }
     bool permissoesPermitidas = await _permissoesPermitidas();
-    if(!permissoesPermitidas){
+    if (!permissoesPermitidas) {
       return;
     }
     Position position = await Geolocator.getCurrentPosition();
     setState(() {
-      _linhas.add('Latitude: ${position?.latitude} | Longitude: ${position?.longitude}');
+      _linhas.add(
+          'Latitude: ${position.latitude} | Longitude: ${position.longitude}');
     });
   }
 
-  void _iniciarMonitoramento(){
-    _subiscription = Geolocator.getPositionStream(
+  void _monitorarLocalizacao() {
+    const LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+      timeLimit: Duration(seconds: 2),
+    );
+    _subscription = Geolocator.getPositionStream(
         locationSettings: locationSettings
     ).listen((Position position) {
       setState(() {
-        _linhas.add('Latitude: ${position?.latitude} | Longitude: ${position?.longitude}');
+        _linhas.add(
+            'Latitude: ${position.latitude} | Longitude: ${position.longitude}');
       });
-      if(_ultimaLocalizacaoObtida != null){
+      if (_ultimaPosicaoObtida != null) {
         final distancia = Geolocator.distanceBetween(
-            _ultimaLocalizacaoObtida!.latitude,
-           _ultimaLocalizacaoObtida!.longitude,
-            position.latitude,
-           position.longitude);
-        distanciaPercorida += distancia;
-        _linhas.add('Distancia percorrida: ${distanciaPercorida.toInt()}m');
+          _ultimaPosicaoObtida!.latitude,
+          _ultimaPosicaoObtida!.longitude,
+          position.latitude,
+          position.longitude,
+        );
+        _distanciaTotalPercorrida += distancia;
+        _linhas.add(
+            'Distância total percorrida: ${_distanciaTotalPercorrida.toInt()}m');
       }
-      _ultimaLocalizacaoObtida = position;
+      _ultimaPosicaoObtida = position;
     });
   }
 
-  void _pararMonitoramento(){
-    _subiscription?.cancel();
+  void _pararMonitoramento() {
+    _subscription?.cancel();
     setState(() {
-      _subiscription = null;
-      distanciaPercorida = 0;
+      _subscription = null;
+      _ultimaPosicaoObtida = null;
+      _distanciaTotalPercorrida = 0;
+    });
+  }
+
+  void _limparLog() {
+    setState(() {
+      _linhas.clear();
     });
   }
 
   Future<bool> _servicoHabilitado() async {
     bool servicoHabilitado = await Geolocator.isLocationServiceEnabled();
-    if(!servicoHabilitado){
-      await _mostrarDialogMensagem('Para utilizar este recurso, você deverá habilitar'
-              ' o serviço de localização do dispostivo');
+    if (!servicoHabilitado) {
+      await _mostrarDialogMensagem('Para utilizar esse recurso, você deverá '
+          'habilitar o serviço de localização do dispositivo');
       Geolocator.openLocationSettings();
       return false;
     }
     return true;
   }
 
-  Future<bool> _permissoesPermitidas() async{
+  Future<bool> _permissoesPermitidas() async {
     LocationPermission permissao = await Geolocator.checkPermission();
-    if(permissao == LocationPermission.denied){
-      permissao = await Geolocator.checkPermission();
-      if(permissao == LocationPermission.denied){
-        _mostrarMensagem(''
-            'Não será possível utilizar o serviço de localização por falta de permissão');
+    if (permissao == LocationPermission.denied) {
+      permissao = await Geolocator.requestPermission();
+      if (permissao == LocationPermission.denied) {
+        _mostrarMensagem(
+            'Não será possível utilizar o recurso por falta de permissão');
         return false;
       }
     }
-    if(permissao == LocationPermission.deniedForever){
+    if (permissao == LocationPermission.deniedForever) {
       await _mostrarDialogMensagem(
-        'Para utilizar esse recurso, você deverá acessar as configurações '
-            'do app e permitir a utilização do serviço de localização');
+          'Para utilizar esse recurso, você deverá acessar as configurações '
+              'do app e permitir a utilização do serviço de localização');
       Geolocator.openAppSettings();
       return false;
     }
     return true;
   }
 
-  void _mostrarMensagem(String mensagem){
+  void _mostrarMensagem(String mensagem) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-       content: Text(mensagem),
+      content: Text(mensagem),
     ));
   }
-  Future<void> _mostrarDialogMensagem(String mensagem) async{
+
+  Future<void> _mostrarDialogMensagem(String mensagem) async {
     await showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-          title: const Text('Atenção'),
-          content: Text(mensagem),
-          actions: [
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ],
-        )
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Atenção'),
+        content: Text(mensagem),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
     );
-  }
-  void _limparLog(){
-    setState(() {
-      _linhas.clear();
-    });
   }
 }
